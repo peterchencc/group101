edfbfd33030db7ffa8b72284748e300b93f04a41
peter_summers <peter@summers.com.tw>
2014-12-26 18:51:29 +0800

    install devise 加入使用者功能

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :development do
 end
 
 gem "simple_form"
+
+gem 'devise'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!  
+
   before_action :find_group
   def new
     # @group = Group.find(params[:group_id])
