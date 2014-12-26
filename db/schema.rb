10f9b778ee433df45b693e6dc971a0414119718d
peter_summers <peter@summers.com.tw>
2014-12-26 17:05:36 +0800

    create posts controller & post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225094735) do
+ActiveRecord::Schema.define(version: 20141226090207) do
 
   create_table "groups", force: true do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20141225094735) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "categories"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
